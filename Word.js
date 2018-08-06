2976e40bb78bdc024577633d8b1bf2b1f866ff94
Schuyler Ankele <schuyler.ankele@charter.com>
2018-08-05 23:57:02 -0600

    A long way out still

--- Word.js
+++ Word.js
@@ -1,6 +1,18 @@
 class Word{
   constructor(){
-  
+    this.selectWord = ()=>{
+      let randomIndex = Math.floor(Math.random() * this.wordbank).length;
+      this.selectedWord = this.wordbank[this.randomIndex];
+      this.unguessed = ("_ ").repeat(this.selectedWord,length);
+    }
+    this.wordbank = ["Refactor", "Python", "Ruby"];
+    this.selectedWord = "";
+    this.unguessed = "";
+    this.correctGuesses = [];
+    this.correctLetters = [];
+    this.fillLetters = ()=>{
+      
+    }
   }
 }
 
--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 const inquirer = require ('inquirer');
 
 // Incorperate the other modules
+
+
+// Start the prompts
+var prompt = inquirer.createPromptModule();
+
+function guessPrompt(){
+    prompt({
+        name: "guessedLetter",
+        message: "Please guess a letter in the word"        
+    }).then(console.log());
+}
