649427e0c69ac44cb85ea070e53fd0c762844b77
Schuyler Ankele <schuyler.ankele@charter.com>
2018-08-07 00:18:23 -0600

    Kind of working.  Need to refactor

--- Word.js
+++ Word.js
@@ -3,12 +3,14 @@ class Word{
     this.wordbank = ["refactor", "python", "ruby"];
     this.selectedWord = "";
     this.unguessed = "";
+    this.answerWord = "";
     this.correctGuesses = [];
     this.correctLetters = [];
     this.selectWord = ()=>{
       let randomIndex = Math.floor(Math.random() * this.wordbank.length);
       this.selectedWord = this.wordbank[randomIndex];
       this.unguessed = ("_ ").repeat(this.selectedWord.length);
+      this.answerWord = (" ").repeat(this.selectedWord.length);
     }
     this.fillLetters = ()=>{
       let theLetters = this.selectedWord.split("");
@@ -16,8 +18,6 @@ class Word{
     }
   }
 }
-var aWord = new Word();
-aWord.selectWord();
-console.log(aWord.selectedWord);
+
 
 module.exports = Word;
--- index.js
+++ index.js
@@ -9,11 +9,13 @@ Letter.correctLetters = Word.fillLetters();
 
 // Incorperate the other modules
 var guessesRemaining = 10
-
+var answer = Word.answerWord;
 // Start the prompts
 var prompt = inquirer.createPromptModule();
 
 function guessPrompt(){
+    
+    console.log(guessesRemaining);
     console.log(`Mystery Word: ${Word.unguessed}`);
     if(guessesRemaining > 0){
         prompt({
@@ -27,15 +29,21 @@ function guessPrompt(){
                 console.log("Correcto!");
                 while(Letter.correctLetters.includes(char)){
                     let letterIndex = Letter.correctLetters.indexOf(char);
-                    Letter.correctLetters[letterIndex] = " ";
-                    Word.unguessed = replaceAt(Word.unguessed, letterIndex * 2, `${char} `);
+                    Letter.correctLetters[letterIndex] = "";
+                    Word.unguessed = replaceAt(Word.unguessed, letterIndex * 2, `${char}`);
+                    answer = replaceAt(answer, letterIndex, `${char}`);
                     console.log(Word.unguessed);
+                    console.log(Word.answerWord);
                 }
             }
             else{
-
+                console.log("Incorrect!")
             }
             console.log(answers);
+            if(answer == Word.selectedWord){
+                console.log("You WIN!!!");
+                return;
+            }
             guessesRemaining--;
             guessPrompt();
         });
@@ -47,17 +55,11 @@ function guessPrompt(){
 }
 guessPrompt();
 
-function swapLetters(char){
-
-}
 
 function replaceAt(string, index, replace) {
     if(index == (string.length-1)){
         console.log('The last char')
     }
     var reply = string.substring(0, index) + replace + string.substring(index + 1);
-    console.log(reply);
     return reply;
-
-
 }
